f83baa4c415581a26994517789bf93244049dac7
ZirconCode <gruenings@gmail.com>
2014-04-19 20:54:49 +0200

    added some stats

--- json_Statistics.py
+++ json_Statistics.py
@@ -1,5 +1,5 @@
 #Addon: JSON statistics
-#Version: 0.0.1
+#Version: 0.0.2
 #Description: Autogenerate a JSON file in your home directory containing statistics about your collections. The file is anki.json.
 
 
@@ -8,15 +8,25 @@ import json
 from aqt import mw
 from aqt.utils import showInfo
 from aqt.qt import *
-from anki.hooks import wrap
+#from anki.hooks import wrap
+#from anki.hooks import addHook
 
 def writeSummary():
     loadCollection()
+
+    data = {}
+    stats = mw.col.stats()
+
+    data['cardCount'] = mw.col.cardCount()
+    data['todayStats'] = stats.todayStats()
+    
     home = expanduser("~")
     filename = home + "/anki.json"
     with open(filename,"w") as f:
-       json.dump({"cardCounts": mw.col.cardCount()},f)
-       f.close()
+        json.dump(data,f)
+        f.close()
 
 loadCollection = mw.loadCollection
 mw.loadCollection = writeSummary
+#addHook("sync", writeSummary) #runHook("sync", "login")
+
